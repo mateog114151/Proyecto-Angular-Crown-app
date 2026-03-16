58ccc5f1b3895af4e34c9c2b9c3e3f3dc46191c2
BoldMart <mgomezgomez2008@gmail.com>
2026-03-16 14:22:12 -0500

    Auto-generated commit

--- chunk-PEVSJKPX.js
+++
@@ -1,1 +0,0 @@
-import{F as s,N as m,b as t,j as i}from"./chunk-Q6FIAAXK.js";var a=[],r=class o{static \u0275fac=function(e){return new(e||o)};static \u0275mod=i({type:o});static \u0275inj=t({imports:[m.forChild(a),m]})};var n=class o{static \u0275fac=function(e){return new(e||o)};static \u0275mod=i({type:o});static \u0275inj=t({imports:[s,r]})};export{n as CastModule};
